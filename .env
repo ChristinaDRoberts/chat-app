820fc975fb3457116871f805c7cc71534ef20a47
Christina <Christinepeninsula@hotmail.com>
2019-03-30 23:37:36 -0400

    conf

--- .env
+++
@@ -1,1 +0,0 @@
-release: python manage.py migrate
--- conf/settings.py
+++ conf/settings.py
@@ -37,6 +37,12 @@ STATICFILES_DIRS = (
 )
 
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME ="chat27"
+AWS_S3_FILE_OVERWRITE= False
+AWS_DEFAULT_ACL =None  # DEFAULT WILL BE TO LOCKDOWN EVERYTHING
 
 
 # Quick-start development settings - unsuitable for production
