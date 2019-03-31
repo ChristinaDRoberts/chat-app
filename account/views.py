3e321f2c089188f29b39aad3f2aabf625a9e38fa
Christina <Christinepeninsula@hotmail.com>
2019-03-30 21:19:58 -0400

    login first

--- account/views.py
+++ account/views.py
@@ -11,6 +11,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import SignUpForm
+from django.views.generic import TemplateView
 # from django.views import RegistrationView
 
 
@@ -18,6 +19,9 @@ class SignUp(generic.CreateView):
    form_class = SignUpForm
    success_url = reverse_lazy('users:login')
    template_name = 'registration/signup.html'
+
+class MarketingView(TemplateView):
+   template_name = "registration/index.html"
 
 
 
