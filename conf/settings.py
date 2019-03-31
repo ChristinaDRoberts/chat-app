a87b46bb4e8af0df857d0a77250d368de318f6ea
Christina <Christinepeninsula@hotmail.com>
2019-03-30 21:05:22 -0400

    deploy

--- conf/settings.py
+++ conf/settings.py
@@ -33,7 +33,7 @@ STATICFILES_FINDERS = [
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'chat/static'),
 )
 
 
