a6cc8b8243665e96108de6227be30a3a59f7a73b
Christina <Christinepeninsula@hotmail.com>
2019-03-30 21:25:58 -0400

    marketing view

--- account/urls.py
+++ account/urls.py
@@ -8,10 +8,12 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 from django.urls import path
+from .views import MarketingView
 
 app_name = "account"
 
 urlpatterns = [
+  path('', MarketingView.as_view(), name="index"),
   path('login/', LoginView.as_view(template_name='registration/login.html'), name="login"),
   path('logout', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
   path('signup/', views.SignUp.as_view(), name='signup'),
